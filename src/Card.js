f4ac4aa0fb006c07d8f2f5b64ca8e6f5bdf132f4
DeyanTopalov <deyan.topalow@gmail.com>
2023-04-09 21:18:11 +0300

    updates

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'; 
+// import './Card.css'
+
+// class Card extends Component {
+
+// } Keep this out for the moment
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
+            <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Card;
