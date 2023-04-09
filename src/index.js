93cca1e131d97e9258c27a76b967246ecdafca7a
DeyanTopalov <deyan.topalow@gmail.com>
2023-04-10 00:43:27 +0300

    update #2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+// import Card from './Card'; not used here anymore - should be removed
+// import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
+// import { robots } from './robots';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Card id ={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id ={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id ={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <App />
   </React.StrictMode>
 );
 
