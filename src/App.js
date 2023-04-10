11fd63dfa77ab8ae80b57a4c8461e84b7e385d60
DeyanTopalov <deyan.topalow@gmail.com>
2023-04-10 03:22:03 +0300

    update #3

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ class App extends Component{
          searchfield: ''
         }
     }
+
+    onSearchChange = (event) => { 
+        this.setState({ searchfield: event.target.value })
+    }
     render() {
+        const filteredRobot = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots}/>
+                <SearchBox searchChange= {this.onSearchChange}/>
+                <CardList robots={filteredRobot}/>
             </div>
             
         );
@@ -27,3 +34,36 @@ class App extends Component{
 }
 
 export default App;
+
+//* Optimized by AI: 
+
+// import React, { Component } from 'react';
+// import CardList from './CardList';
+// import { robots } from './robots';
+// import SearchBox from './SearchBox';
+
+// class App extends Component {
+//     state = {
+//         robots: robots,
+//         searchfield: ''
+//     }
+
+//     onSearchChange = (event) => {
+//         this.setState({ searchfield: event.target.value.toLowerCase() })
+//     }
+
+//     render() {
+//         const { robots, searchfield } = this.state;
+//         const filteredRobot = robots.filter(robot => robot.name.toLowerCase().includes(searchfield));
+        
+//         return (
+//             <div className='tc'>
+//                 <h1>RoboFriends</h1>
+//                 <SearchBox searchChange={this.onSearchChange}/>
+//                 <CardList robots={filteredRobot}/>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
