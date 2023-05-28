bad3cfe02d92dad0a2c085f341f238cadaf88465
DeyanTopalov <deyan.topalow@gmail.com>
2023-05-28 23:31:19 +0300

    updates

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,48 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import './App.css';
-import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
+import React, { useState, useEffect } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import "./App.css";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+  //* basicaly does the same as above, we are destructuring an array
+  //* basically 'robots' is a piece of state and
+  //* 'setRobots' is the function that changes the state (this is the
+  //* naming convention, if the state was items the function is to be
+  //* named setItems)
+  //* before we had one big state that contained the robots and searchfield
+  //* now we have 2 smaller states - 1 for robots etc..
 
-class App extends Component{
-    constructor() {
-    super()
-    this.state = {
-        robots: [],
-         searchfield: ''
-        }
-    }
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  }, []); // we need this array as second parameter, otherwise
+  // it will go into an infinete loop. In the array we say "hey man,
+  // setRobots (update the state) only when the List (the empty array) changes"
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json())
-        .then(users=>this.setState({robots: users}));
-    }
-
-    onSearchChange = (event) => { 
-        this.setState({ searchfield: event.target.value })
-    }
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobot = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
-            return !robots.length ? //changed
-                <h1>Loading</h1> :
-                (
-                <div className='tc'>
-                    <h1 className ='f1'>Robo Friends</h1>
-                    <SearchBox searchChange= {this.onSearchChange}/>
-                    <Scroll> 
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobot}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-        }
-    }
- 
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
+  const filteredRobot = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">Robo Friends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filteredRobot} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
+}
 
 export default App;
